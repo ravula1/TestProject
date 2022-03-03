8e1e46f1a610065a1bceb3fcc5dbe3565a25fa79
Administrator <Administrator@RAVULAS.lan>
2022-03-02 19:27:38 -0500

    Added New Class

---
+++ myLocalRepository/HelloWorld/src/com/hasiinfotechllc/demo/hr/testWorld.java
@@ -0,0 +1,10 @@
+package com.hasiinfotechllc.demo.hr;
+
+public class testWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.println("Second class");
+	}
+
+}
