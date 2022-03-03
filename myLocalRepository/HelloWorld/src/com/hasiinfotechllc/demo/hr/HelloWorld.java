a7c7a924ed9ec9a6509faf99f1cb65739985fa72
Administrator <Administrator@RAVULAS.lan>
2022-03-02 19:24:30 -0500

    Added New output message.

---
+++ myLocalRepository/HelloWorld/src/com/hasiinfotechllc/demo/hr/HelloWorld.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package com.hasiinfotechllc.demo.hr;
+import java.lang.*;
+
+/**
+ * @author Administrator
+ *
+ */
+@SuppressWarnings("unused")
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!!!");
+		System.out.println("test2");
+
+	}
+
+}
